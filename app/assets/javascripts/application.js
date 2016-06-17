454552fbc470a3eecd75e8796a2aef13dc14be2c
Arturo Perez <VrturoPerez@gmail.com>
2016-06-16 20:13:14 -0700

    asset pipeline misfiring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//
+//= script
+//= lib_folder
 //= require_tree .
