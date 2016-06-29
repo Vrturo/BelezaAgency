f2ac1fd773f567cfcca8de562534503a44ac4571
Arturo Perez <VrturoPerez@gmail.com>
2016-06-29 13:48:39 -0700

    add paperclip initializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,36 @@ class User < ActiveRecord::Base
     validates :first_name, :last_name, :email, presence: true
     validates :email, uniqueness: true
 
-    has_many :attachments
-    accepts_nested_attributes_for :attachments
+    # has_many :attachments
+    # accepts_nested_attributes_for :attachments
 
-    has_attached_file :photo_one, :photo_two, :photo_three, :photo_four, styles: {
+    has_attached_file :photo_one, styles: {
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
-  }
+    }
+
+    has_attached_file :photo_two, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+    }
+
+    has_attached_file :photo_three, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+    }
+
+    has_attached_file :photo_four, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+    }
 
   # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :photo_one, :photo_two, :photo_three, :photo_four, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :photo_one, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :photo_two, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :photo_three, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :photo_four, :content_type => /\Aimage\/.*\Z/
 end
---
+++ config/initializers/paper_clip.rb
@@ -0,0 +1,4 @@
+Paperclip::Attachment.default_options[:url] = ENV['S3_DOMAIN_URL']
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = ENV['S3_HOST_NAME']
+
