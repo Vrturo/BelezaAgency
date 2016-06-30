b5c260a14d3ab474861045285578a12392e33730
Arturo Perez <VrturoPerez@gmail.com>
2016-06-30 01:04:42 -0700

    add required fields

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
       session[:user_id] = nil
-      flash[:notice] = "You are now logged out"
       redirect_to "/"
     end
 end
