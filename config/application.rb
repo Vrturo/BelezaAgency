ccd76225f85b7410ebfefa075c2e4d907c312599
Arturo Perez <VrturoPerez@gmail.com>
2016-11-28 22:26:50 -0800

    create fonts its own asets folder

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module BelezaAgency
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
