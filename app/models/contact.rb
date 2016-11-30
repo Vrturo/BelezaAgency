7d5eeed1a24601903d97c2339bbc8b61a4e77f44
Arturo Perez <VrturoPerez@gmail.com>
2016-11-29 22:20:47 -0800

    add contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,1 +1,15 @@
-contact.rb
+class Contact < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "Beleza Contact Form",
+      :to => "VrturoPerez@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
