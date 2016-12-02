87aeb8abbebe68647ec3b66c9c5c76e2d66e5fcc
Arturo Perez <VrturoPerez@gmail.com>
2016-12-01 18:29:48 -0800

    edit resume look && upload new resume, func doesnt work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,15 +51,12 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        edirect_to @user, notice: 'User was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        flash.now.alert = "could not upload successfully"
+        render :edit
       end
-    end
   end
 
   # DELETE /users/1
