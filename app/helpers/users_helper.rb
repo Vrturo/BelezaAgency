899a533b17ee90ba9fdfa41f593f9620f551cba1
Arturo Perez <VrturoPerez@gmail.com>
2016-12-24 15:52:22 -0800

    remove helper function, add rake task

--- app/helpers/users_helper.rb
+++
@@ -1,7 +0,0 @@
-module UsersHelper
-  def add_attachment_link(name)
-    link_to_function name do |page|
-      page.insert_html :bottom, :attachments, :partial => 'attachment', :object => Attachment.new
-      end
-    end
-end
